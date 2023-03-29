35072914701a57e547be86c64a911917755f42b9
dicksonmutwiriguantai <dickson.mutwiri@student.moringaschool.com>
2023-03-29 07:10:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const num3 = 24
+const num4 = 5;
+const multiply = num1*num2;
+const random = (Number.isInteger(Math.random())+10);
+const mod = num3%num4;
+const max = Math.max(14,9,6,20,17,11);
